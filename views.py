d68e8065b3f7cfb3fd266838b3c5da8ef5e777de
Susruth Puppala <abhirama99@live.com>
2024-02-14 19:40:22 +0000

    Adds rendered html for about and home

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint(__name__,"views")
 
 @views.route('/')
 def home():
-    return "Hello World"
+    return render_template("index.html")
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
