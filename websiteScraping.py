3aacda7d7196b50a92422b1331aa99eaad3c27c0
Susruth Puppala <abhirama99@live.com>
2024-02-17 17:51:05 +0000

    Scrapes all florida man enteries and writes them to a json file

--- websiteScraping.py
+++ websiteScraping.py
@@ -2,30 +2,39 @@ from bs4 import *
 import lxml
 import requests
 from selenium import webdriver
+import json
 
-url = "https://www.huffpost.com/topic/florida-man?page=1"
-
-response = requests.get(url)
+listOfData = []
 
 
-def print_headline(response_text):
-    soup = BeautifulSoup(response_text, "lxml")
-    headlines = soup.find_all(attrs={"class": "card__headline__text"})
-    for headline in headlines:
-        if "Florida Man" in headline.text:
-            print(headline['href'])
-            print(headline.text)
+def headlineFinder(link):
+    for pageNumber in range(1, 7, 1):
+        url = link + str(pageNumber)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "lxml")
+        headlines = soup.find_all(attrs={"class": "card__headline__text"})
+        for headline in headlines:
+            if "Florida Man" in headline.text:
+                listOfData.append([headline.text])
 
-def headline_URL(response_text):
+headlineFinder("https://www.huffpost.com/topic/florida-man?page=")
+
+
+jsonString = json.dumps(listOfData, indent=2)
+
+with open("data.json", "w") as outfile:
+    outfile.write(jsonString)
+
+print(jsonString)
+
+
+# Still needs some work
+"""def headline_URL(response_text):
     soup = BeautifulSoup(response_text, "lxml")
     linksForHeadlines = soup.find_all(attrs={"class": "card__headline card__headline--long"})
     for linksForHeadline in linksForHeadlines:
-        print(linksForHeadline['href'])
-
-
-print_headline(response.text)
-
-headline_URL(response.text)
+            print(linksForHeadline['href'])
+"""
 
 driver_path = "chromedriver"
 brave_path = "/usr/bin/brave-browser"
@@ -33,6 +42,6 @@ brave_path = "/usr/bin/brave-browser"
 option = webdriver.ChromeOptions()
 option.binary_location = brave_path
 
-#browser = webdriver.Chrome(options=option)
+# browser = webdriver.Chrome(options=option)
 
-#browser.get(url)
+# browser.get(url)
