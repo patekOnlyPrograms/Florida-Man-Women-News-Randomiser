6088a2937550ba331649902ca5ae256fb8b4fdd9
Susruth Puppala <abhirama99@live.com>
2024-03-17 18:24:15 +0000

    managed to get text to display after clicked button

---
+++ static/script.js
@@ -0,0 +1,7 @@
+let name = document.querySelector("randomiseOutput").onclick = randomiseFloridaMan()
+let json= fetch("static/data.json")
+
+function randomiseFloridaMan(){
+    document.getElementById("headline-txt").innerText = "Placeholder for headlines";
+
+}
